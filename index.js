cd66811ef7e51ea22f0dae415bf06dad6822d775
khanz1 <assistance.xavier@gmail.com>
2025-03-04 13:45:57 +0700

    feat-login

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
 const express = require("express");
 const app = express();
+const { User } = require("./models");
+const { comparePassword } = require("./helpers/bcrypt");
+const { signToken } = require("./helpers/jwt");
+
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email) {
+      throw { name: "BadRequest", message: "Email is required" };
+    }
+    if (!password) {
+      throw { name: "BadRequest", message: "Password is required" };
+    }
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      throw { name: "Unauthorized", message: "Email or password is required" };
+    }
+
+    const isValidPassword = comparePassword(password, user.password);
+    if (!isValidPassword) {
+      throw { name: "Unauthorized", message: "Email or password is required" };
+    }
+
+    const access_token = signToken({ id: user.id });
+    res.status(200).json({ access_token });
+  } catch (err) {
+    if (err.name === "BadRequest") {
+      res.status(400).json({ message: err.message });
+    } else if (err.name === "Unauthorized") {
+      res.status(401).json({ message: err.message });
+    } else {
+      res.status(500).json({ message: "Internal server error" });
+    }
+  }
+});
+
 app.listen(3000, () => console.log("Server running on port 3000"));
